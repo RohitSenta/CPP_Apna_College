819130d3cb312c1ea9ed4f5214452439a23472bc
RohitSenta <rohitsenta@gmail.com>
2023-05-30 22:59:28 +0530

    Factorial of Number Using Function

---
+++ Factorial-of-Number-Using-Function.cpp
@@ -0,0 +1,23 @@
+// Factorial of Number Using Function
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    int factorial = 1;
+    for(int i=2; i<=n; i++){
+        factorial = factorial*i;
+    }
+    return factorial;
+}
+
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    int ans = fact(n);
+    cout << ans << endl;
+
+    return 0;
+}
